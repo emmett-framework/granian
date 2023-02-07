ae3e3f10c97a3b45440d42f6c61dad9588412486
Giovanni Barillari <giovanni.barillari@gmail.com>
2023-02-07 01:10:09 +0100

    Drop `pyo3-asyncio` fork (#56)

--- lib/pyo3-asyncio/.githooks/pre-commit
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cargo check --all-targets --all-features
--- lib/pyo3-asyncio/src/err.rs
+++
@@ -1,9 +0,0 @@
-// FIXME - is there a way to document custom PyO3 exceptions?
-#[allow(missing_docs)]
-mod exceptions {
-    use pyo3::{create_exception, exceptions::PyException};
-
-    create_exception!(pyo3_asyncio, RustPanic, PyException);
-}
-
-pub use exceptions::RustPanic;
