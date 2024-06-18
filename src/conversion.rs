749bc24b02bf4af290bea3e5fce757bd92897549
Giovanni Barillari <g@baro.dev>
2024-06-19 00:53:47 +0200

    Enhance WSGI iterators (#332)

--- src/conversion.rs
+++ src/conversion.rs
@@ -1,37 +1,20 @@
 use pyo3::prelude::*;
-use std::ops::{Deref, DerefMut};
 
 use crate::workers::{HTTP1Config, HTTP2Config};
 
 pub(crate) struct BytesToPy(pub hyper::body::Bytes);
 
-impl Deref for BytesToPy {
-    type Target = hyper::body::Bytes;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for BytesToPy {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
 impl IntoPy<PyObject> for BytesToPy {
     #[inline]
     fn into_py(self, py: Python) -> PyObject {
-        (&self[..]).into_py(py)
+        self.0.as_ref().into_py(py)
     }
 }
 
 impl ToPyObject for BytesToPy {
     #[inline]
     fn to_object(&self, py: Python<'_>) -> PyObject {
-        (&self[..]).into_py(py)
+        self.0.as_ref().into_py(py)
     }
 }
 
