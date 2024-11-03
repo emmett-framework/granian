3daf12b189f94c09438564b05c1c4500db571bc9
Pepe Navarro <pepe@pensero.ai>
2024-11-03 20:01:00 +0100

    On reload, ignore non python files by default

--- granian/server.py
+++ granian/server.py
@@ -629,19 +629,31 @@ class Granian:
             logger.error('Using --reload requires the granian[reload] extra')
             sys.exit(1)
 
-        # Use given or default filter rules
-        reload_filter = self.reload_filter or watchfiles.filters.DefaultFilter
-        # Extend `reload_filter` with explicit args
-        ignore_dirs = (*reload_filter.ignore_dirs, *self.reload_ignore_dirs)
-        ignore_entity_patterns = (
-            *reload_filter.ignore_entity_patterns,
-            *self.reload_ignore_patterns,
-        )
-        ignore_paths = (*reload_filter.ignore_paths, *self.reload_ignore_paths)
-        # Construct new filter
-        reload_filter = watchfiles.filters.DefaultFilter(
-            ignore_dirs=ignore_dirs, ignore_entity_patterns=ignore_entity_patterns, ignore_paths=ignore_paths
-        )
+        if self.reload_ignore_dirs or self.reload_ignore_patterns:
+            # Use given or default filter rules
+            reload_filter = self.reload_filter or watchfiles.filters.DefaultFilter
+            # Extend `reload_filter` with explicit args
+            ignore_dirs = (*reload_filter.ignore_dirs, *self.reload_ignore_dirs)
+            ignore_entity_patterns = (
+                *reload_filter.ignore_entity_patterns,
+                *self.reload_ignore_patterns,
+            )
+            ignore_paths = (*reload_filter.ignore_paths, *self.reload_ignore_paths)
+            # Construct new filter
+            reload_filter = watchfiles.filters.DefaultFilter(
+                ignore_dirs=ignore_dirs,
+                ignore_entity_patterns=ignore_entity_patterns,
+                ignore_paths=ignore_paths
+            )
+        else:
+            # Use given or python filter rules
+            reload_filter = self.reload_filter or watchfiles.filters.PythonFilter
+            # Extend `reload_filter` with explicit args
+            ignore_paths = (*reload_filter.ignore_paths, *self.reload_ignore_paths)
+            # Construct new filter
+            reload_filter = watchfiles.filters.PythonFilter(
+                ignore_paths=ignore_paths
+            )
 
         sock = self.startup(spawn_target, target_loader)
 
