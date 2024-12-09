1ee0d5f10879aeca53fe980e88a85bbb9d79d34a
Hendrik Muhs <hendrik.muhs@gmail.com>
2024-12-09 20:32:17 +0100

    revert tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,13 +11,13 @@ import pytest
 from granian import Granian
 
 
-def _serve(app, **kwargs):
-    server = Granian(f'tests.apps.{app}:app', **kwargs)
+def _serve(**kwargs):
+    server = Granian(f'tests.apps.{kwargs["interface"]}:app', **kwargs)
     server.serve()
 
 
 @asynccontextmanager
-async def _server(interface, app, port, threading_mode, tls=False, extra_args=None):
+async def _server(interface, port, threading_mode, tls=False):
     certs_path = Path.cwd() / 'tests' / 'fixtures' / 'tls'
     kwargs = {
         'interface': interface,
@@ -25,21 +25,15 @@ async def _server(interface, app, port, threading_mode, tls=False, extra_args=No
         'threading_mode': threading_mode,
         'loop_opt': bool(os.getenv('LOOP_OPT')),
     }
-    if extra_args:
-        kwargs.update(extra_args)
     if tls:
-        if tls == 'private':
-            kwargs['ssl_cert'] = certs_path / 'pcert.pem'
-            kwargs['ssl_key'] = certs_path / 'pkey.pem'
-            kwargs['ssl_key_password'] = 'foobar'  # noqa: S105
-        else:
-            kwargs['ssl_cert'] = certs_path / 'cert.pem'
-            kwargs['ssl_key'] = certs_path / 'key.pem'
+        kwargs['ssl_cert'] = certs_path / 'cert.pem'
+        kwargs['ssl_key'] = certs_path / 'key.pem'
 
     succeeded, spawn_failures = False, 0
     while spawn_failures < 3:
-        proc = mp.get_context('spawn').Process(target=_serve, args=(app,), kwargs=kwargs)
+        proc = mp.get_context('spawn').Process(target=_serve, kwargs=kwargs)
         proc.start()
+
         conn_failures = 0
         while conn_failures < 3:
             try:
@@ -77,29 +71,24 @@ def server_port():
 
 @pytest.fixture(scope='function')
 def asgi_server(server_port):
-    return partial(_server, 'asgi', 'asgi', server_port)
+    return partial(_server, 'asgi', server_port)
 
 
 @pytest.fixture(scope='function')
 def rsgi_server(server_port):
-    return partial(_server, 'rsgi', 'rsgi', server_port)
+    return partial(_server, 'rsgi', server_port)
 
 
 @pytest.fixture(scope='function')
 def wsgi_server(server_port):
-    return partial(_server, 'wsgi', 'wsgi', server_port)
+    return partial(_server, 'wsgi', server_port)
 
 
 @pytest.fixture(scope='function')
 def server(server_port, request):
-    return partial(_server, request.param, request.param, server_port)
+    return partial(_server, request.param, server_port)
 
 
 @pytest.fixture(scope='function')
 def server_tls(server_port, request):
-    return partial(_server, request.param, request.param, server_port, tls=True)
-
-
-@pytest.fixture(scope='function')
-def server_app(server_port):
-    return partial(_server, port=server_port)
+    return partial(_server, request.param, server_port, tls=True)
