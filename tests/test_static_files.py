4ae755e4c16191ec15ed0a889443e733a1f3fa59
Molly Rose <molly@mollymorphous.dev>
2025-07-21 18:35:22 -0400

    Unit test for precompressed static files

--- tests/test_static_files.py
+++ tests/test_static_files.py
@@ -44,3 +44,47 @@ async def test_static_files_approute(server_static_files, runtime_mode):
         res = httpx.get(f'http://localhost:{port}/info')
 
     assert res.status_code == 200
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('server_static_files', ['asgi', 'rsgi', 'wsgi'], indirect=True)
+@pytest.mark.parametrize('runtime_mode', ['mt', 'st'])
+async def test_static_files_precompressed_identity(server_static_files, runtime_mode):
+    async with server_static_files(runtime_mode, ws=False) as port:
+        res = httpx.get(f'http://localhost:{port}/static/precompressed.txt', headers={'accept-encoding': ''})
+
+    assert res.status_code == 200
+    assert 'content-encoding' not in res.headers
+    assert 'vary' not in res.headers
+
+
+@pytest.mark.xfail
+@pytest.mark.asyncio
+@pytest.mark.parametrize('server_static_files', ['asgi', 'rsgi', 'wsgi'], indirect=True)
+@pytest.mark.parametrize('runtime_mode', ['mt', 'st'])
+async def test_static_files_precompressed_gzip(server_static_files, runtime_mode):
+    async with server_static_files(runtime_mode, ws=False) as port:
+        res = httpx.get(
+            f'http://localhost:{port}/static/precompressed.txt',
+            headers={'accept-encoding': 'gzip, deflate'},
+        )
+
+    assert res.status_code == 200
+    assert res.headers['content-encoding'] == 'gzip'
+    assert res.headers['vary'] == 'accept-encoding'
+
+
+@pytest.mark.xfail
+@pytest.mark.asyncio
+@pytest.mark.parametrize('server_static_files', ['asgi', 'rsgi', 'wsgi'], indirect=True)
+@pytest.mark.parametrize('runtime_mode', ['mt', 'st'])
+async def test_static_files_precompressed_brotli(server_static_files, runtime_mode):
+    async with server_static_files(runtime_mode, ws=False) as port:
+        res = httpx.get(
+            f'http://localhost:{port}/static/precompressed.txt',
+            headers={'accept-encoding': 'gzip, deflate, br'},
+        )
+
+    assert res.status_code == 200
+    assert res.headers['content-encoding'] == 'br'
+    assert res.headers['vary'] == 'accept-encoding'
