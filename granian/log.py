48bbe9e88510cee94718c48c77d8e86d85b395d7
focus zheng <zffocus@gmail.com>
2025-08-12 18:25:54 +0800

    atoms

--- granian/log.py
+++ granian/log.py
@@ -7,6 +7,28 @@ from enum import Enum
 from typing import Any, Dict, Optional
 
 
+class SafeAtoms(dict):
+    def __init__(self, atoms):
+        dict.__init__(self)
+        for key, value in atoms.items():
+            if isinstance(value, str):
+                self[key] = value.replace('"', '\\"')
+            else:
+                self[key] = value
+
+    def __getitem__(self, k):
+        if k.startswith('{'):
+            kl = k.lower()
+            if kl in self:
+                return super().__getitem__(kl)
+            else:
+                return '-'
+        if k in self:
+            return super().__getitem__(k)
+        else:
+            return '-'
+
+
 class LogLevels(str, Enum):
     critical = 'critical'
     error = 'error'
@@ -43,8 +65,16 @@ LOGGING_CONFIG = {
         },
     },
     'handlers': {
-        'console': {'formatter': 'generic', 'class': 'logging.StreamHandler', 'stream': 'ext://sys.stdout'},
-        'access': {'formatter': 'access', 'class': 'logging.StreamHandler', 'stream': 'ext://sys.stdout'},
+        'console': {
+            'formatter': 'generic',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
+        'access': {
+            'formatter': 'access',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+        },
     },
     'loggers': {
         '_granian': {'handlers': ['console'], 'level': 'INFO', 'propagate': False},
@@ -52,14 +82,18 @@ LOGGING_CONFIG = {
     },
 }
 
-DEFAULT_ACCESSLOG_FMT = '[%(time)s] %(addr)s - "%(method)s %(path)s %(protocol)s" %(status)d %(dt_ms).3f'
+DEFAULT_ACCESSLOG_FMT = (
+    '[%(time)s] %(addr)s - "%(method)s %(path)s %(protocol)s" %(status)d %(dt_ms).3f'
+)
 
 # NOTE: to be consistent with the Rust module logger name
 logger = logging.getLogger('_granian')
 access_logger = logging.getLogger('granian.access')
 
 
-def configure_logging(level: LogLevels, config: Optional[Dict[str, Any]] = None, enabled: bool = True):
+def configure_logging(
+    level: LogLevels, config: Optional[Dict[str, Any]] = None, enabled: bool = True
+):
     log_config = copy.deepcopy(LOGGING_CONFIG)
 
     if config:
@@ -80,19 +114,31 @@ def log_request_builder(fmt):
     def log_request(rtime, req, res_code):
         dt = time.time() - rtime
         rdt = datetime.datetime.fromtimestamp(rtime, tz=local_tz)
+        atoms_context = {
+            'addr': req['addr_remote'],
+            'time': rdt.strftime('%Y-%m-%d %H:%M:%S %z'),
+            'dt_ms': dt * 1000,
+            'status': res_code,
+            'path': req['path'],
+            'query_string': req['qs'],
+            'method': req['method'],
+            'scheme': req['scheme'],
+            'protocol': req['protocol'],
+            # request body length
+            'rbl': req['response_body_length'],
+        }
+        context = SafeAtoms(atoms_context)
+        del req['addr_remote']
+        del req['path']
+        del req['qs']
+        del req['method']
+        del req['scheme']
+        del req['protocol']
+        del req['response_body_length']
+        context.update(req)
         access_logger.info(
             fmt,
-            {
-                'addr': req['addr_remote'],
-                'time': rdt.strftime('%Y-%m-%d %H:%M:%S %z'),
-                'dt_ms': dt * 1000,
-                'status': res_code,
-                'path': req['path'],
-                'query_string': req['qs'],
-                'method': req['method'],
-                'scheme': req['scheme'],
-                'protocol': req['protocol'],
-            },
+            context,
         )
 
     return log_request
