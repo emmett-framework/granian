be439283bbe4f5855d710884a4a5f7f0653ade5a
Giovanni Barillari <giovanni.barillari@sentry.io>
2025-09-01 12:08:14 +0200

    nit

--- granian/__init__.py
+++ granian/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['__version__', 'loops', 'Granian']
-
 from ._granian import __version__
 from ._loops import loops
 from .server import Server as Granian
+
+
+__all__ = ['__version__', 'loops', 'Granian']
