a3ec9a47ffcf2d365b8d07da1195420cc7a06312
Anuraag (Rag) Agrawal <anuraaga@gmail.com>
2025-09-01 19:19:35 +0900

    Define `__all__` in package init (#671)

--- granian/__init__.py
+++ granian/__init__.py
@@ -1,3 +1,6 @@
-from ._granian import __version__  # noqa: F401
-from ._loops import loops  # noqa: F401
-from .server import Server as Granian  # noqa: F401
+from ._granian import __version__
+from ._loops import loops
+from .server import Server as Granian
+
+
+__all__ = ['__version__', 'loops', 'Granian']
